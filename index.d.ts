70ccb8c7f5890e9ee8e2b5e444e1f5208041a4f2
Nanoo <45440100+Calemy@users.noreply.github.com>
2022-10-18 21:19:22 +0200

    fix & update types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,15 @@
-export = class mysqlCommands {
-    constructor(connection: ConnectionAwait)
+class mysqlCommands {
+    createConnection({ host: string, user: string, password: string, database: string }): connectionCommands
+}
+
+class connectionCommands {
     async request(query: string): Promise<Array<Object>>
     async select(table: string, opts?: { values?: Array<String>, condition?: string, sort?: Array<string>, limit?: number, offset?: number}): Promise<Array<Object>>
     async selectOne(table: string, opts?: { values?: Array<String>, condition?: string, sort?: Array<string> }): Promise <Object | undefined>
-    async insert(table: string, opts: { fields: Array<string>, values: Array<string | number>, object: Object<>}): void
-    async update(table: string, opts: { fields: Array<string>, values: Array<string | number>, object: Object<>, condition?: string }): void
+    async insert(table: string, opts: { fields?: Array<string>, values?: Array<string | number>, object?: Object}): Promise<Object>
+    async update(table: string, opts: { fields?: Array<string>, values?: Array<string | number>, object?: Object, condition?: string }): Promise<Object>
     //TODO:
     //* async join(from: string, join: string, on: Array<string>, opts?: { values?: Array<string>, condition?: string, sort?: Array<string>, limit?: number, offset?: number}): Promise<Array<Object>>
 }
+
+export = new mysqlCommands
