32b25632f3243ee6979166687094294da3382df1
Nanoo <45440100+Calemy@users.noreply.github.com>
2023-02-19 16:22:40 +0100

    add pools + minor code change

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 class mysqlCommands {
     createConnection({ host: string, user: string, password: string, database: string }): connectionCommands
+    createPool({ host: string, user: string, password: string, database: string }): poolCommands
 }
 
 class connectionCommands {
@@ -8,8 +9,13 @@ class connectionCommands {
     async selectOne(table: string, opts?: { values?: Array<String>, condition?: string, sort?: Array<string> }): Promise <object | undefined>
     async insert(table: string, opts: { fields?: Array<string>, values?: Array<string | number>, object?: object, updateOnDuplicate?: boolean }): Promise<object>
     async update(table: string, opts: { fields?: Array<string>, values?: Array<string | number>, object?: object, condition?: string }): Promise<object>
+    close(): void
     //TODO:
     //* async join(from: string, join: string, on: Array<string>, opts?: { values?: Array<string>, condition?: string, sort?: Array<string>, limit?: number, offset?: number}): Promise<Array<object>>
+}
+
+class poolCommands extends connectionCommands {
+
 }
 
 export = new mysqlCommands
--- index.js
+++ index.js
@@ -4,42 +4,10 @@ class connectionCommands {
     constructor(){
         this.connection = mysql.createConnection(...arguments);
         this.config = this.connection.config;
-    }
-
-    connect(){
-        this.connection = new connectionCommands(this.config).connection
-    }
-
-    async request(query){
-        this.connect()
-        const request = this.connection.awaitQuery(query)
-        await this.connection.awaitEnd()
-        return request
-    }
-
-    async requestOne(query){
-        this.connect()
-        const request = (await this.connection.awaitQuery(query))[0]
-        await this.connection.awaitEnd()
-        return request
-    }
-
-    async select(table, { values = ["*"], condition, sort, limit = 0, offset = 0 }){
-        return this.request(`
-        SELECT ${values.join(', ')} FROM ${table}
-        ${condition?.length > 0 ? "WHERE " + condition : ""}
-        ${sort?.length > 0 ? " ORDER BY " + sort.join(", ") : ""}
-        ${limit > 0 ? " LIMIT " + limit : ""}
-        ${offset > 0 ? " OFFSET " + offset : ""}
-        `)
-    }
-
-    async selectOne(table, { values = ["*"], condition, sort }){
-        return this.requestOne(`
-        SELECT ${values.join(', ')} FROM ${table}
-        ${condition?.length > 0 ? "WHERE " + condition : ""}
-        ${sort?.length > 0 ? " ORDER BY " + sort.join(", ") : ""}
-        `)
+        this.request = async (query) => await this.connection.awaitQuery(query)
+        this.requestOne = async (query) => (await this.connection.awaitQuery(query))[0]
+        this.select = async (table, { values = ["*"], condition, sort, limit = 0, offset = 0 }) => await this.request(`SELECT ${values.join(', ')} FROM ${table} ${condition?.length > 0 ? "WHERE " + condition : ""}${sort?.length > 0 ? " ORDER BY " + sort.join(", ") : ""}${limit > 0 ? " LIMIT " + limit : ""}${offset > 0 ? " OFFSET " + offset : ""}`)
+        this.selectOne = async (table, { values = ["*"], condition, sort, prepared = [] }) => await this.requestOne(`SELECT ${values.join(', ')} FROM ${table} ${condition?.length > 0 ? "WHERE " + condition : ""}${sort?.length > 0 ? " ORDER BY " + sort.join(", ") : ""}`)
     }
 
     async insert(table, { fields, values, object, updateOnDuplicate = false }){
@@ -96,10 +64,33 @@ class connectionCommands {
         return await this.request(`UPDATE ${table} SET ${str} ${condition?.length > 0 ? "WHERE " + condition : ""}`)
     }
 
+    close() {
+        return new Promise((resolve, reject) => {
+          this.connection.end((err) => {
+            if (err) {
+              this.connection.emit(`error`, err);
+              return this.config.throwErrors ? reject(err) : resolve()
+            }
+            resolve();
+          });
+        });
+      }
+
+}
+
+class poolCommands extends connectionCommands {
+    constructor(){
+        super(...arguments)
+        this.connection = mysql.createPool(...arguments);
+        this.config = this.connection.config;
+    }
 }
 
 module.exports = new class mysqlCommands {
     createConnection(){
         return new connectionCommands(...arguments);
+    }
+    createPool(){
+        return new poolCommands(...arguments);
     }
 }
